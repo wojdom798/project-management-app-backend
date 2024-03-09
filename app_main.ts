2aee43922c13f7bdac5fb97897f5390fb298069b
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2024-03-09 18:05:12 +0100

    chore: implement reading debug data from JSON file and sending it in an api endpoint
    The debug data contains a list of project data (list of projects)
    
    I also defined the IDebugData, IProject, and IFeature types (interfaces)

--- app_main.ts
+++ app_main.ts
@@ -1,11 +1,24 @@
+import fs from "fs";
 import path from "path";
 import express, { Request, Response, NextFunction } from "express";
+import { IDebugData } from "./types/sharedTypes";
 
 function logUrlInfoMiddleware(req: Request, res: Response, next: NextFunction)
 {
     let currentTime = new Date().toLocaleString("pl-PL",{ hour12: false });
     console.log(`[${req.method}] (${currentTime}) ${req.originalUrl}`);
     next();
+}
+
+let debugData: IDebugData | null = null;
+if (
+    typeof process.env.PROJECT_MANAGEMENT_APP_DEBUG_DATA == "string" &&
+    fs.existsSync(process.env.PROJECT_MANAGEMENT_APP_DEBUG_DATA)
+)
+{
+    debugData = JSON.parse(
+        fs.readFileSync(process.env.PROJECT_MANAGEMENT_APP_DEBUG_DATA, "utf-8")
+    ) as IDebugData;
 }
 
 const app = express();
@@ -35,6 +48,30 @@ function (req: Request, res: Response, next: NextFunction)
     res.json({
         message: "API test endpoint success",
     });
+});
+
+
+app.get("/api/get-debug-data",
+function (req: Request, res: Response, next: NextFunction)
+{
+    res.json(debugData);
+});
+
+
+app.get("/api/refresh-debug-data",
+function (req: Request, res: Response, next: NextFunction)
+{
+    if (
+        typeof process.env.PROJECT_MANAGEMENT_APP_DEBUG_DATA == "string" &&
+        fs.existsSync(process.env.PROJECT_MANAGEMENT_APP_DEBUG_DATA)
+    )
+    {
+        debugData = JSON.parse(
+            fs.readFileSync(process.env.PROJECT_MANAGEMENT_APP_DEBUG_DATA, "utf-8")
+        ) as IDebugData;
+    }
+
+    res.json({ msg: "refreshed debug data" });
 });
 
 
