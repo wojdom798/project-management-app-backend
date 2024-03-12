9a734794c668741a4b4604361cfdf1bb64f208db
Dominik Wojciechowski <dominik.wojciechowski798@gmail.com>
2024-03-12 17:50:57 +0100

    feat: add Task list to debug data and update type definitions

--- types/sharedTypes.ts
+++ types/sharedTypes.ts
@@ -1,18 +1,31 @@
-export interface IFeature
-{
-    name: string;
-    priority: number;
-    isFinished: boolean;
-};
-
 export interface IProject
 {
+    id: number;
     name: string;
     description: string;
     features: IFeature[];
 };
 
+
+export interface IFeature
+{
+    id: number;
+    name: string;
+    priority: number;
+    isFinished: boolean;
+};
+
+
+export interface ITask
+{
+    id: number;
+    feature_id: number;
+    name: string;
+};
+
+
 export interface IDebugData
 {
-    projects: IProject[]
+    projects: IProject[];
+    tasks: ITask[];
 };
